b9e25326a68fd8a4553d24b980bb89e10ebf702e
zhantan <zhantan2015@gmail.com>
2022-07-28 20:54:51 +0800

    tags只保留前三个

--- src/app.ts
+++ src/app.ts
@@ -11,17 +11,18 @@ const app = express()
 const port = 3333
 
 
-app.use(bodyParser.urlencoded({ extended: false }))
-if (mode === 'dev')
-    app.all('*', (req, res, next) => {
-        // 设置允许跨域的域名,*代表允许任意域名跨域
-        res.header('Access-Control-Allow-Origin', '*');
-        // 允许的header类型
-        res.header('Access-Control-Allow-Headers', '*');
-        // 跨域允许的请求方式
-        res.header('Access-Control-Allow-Methods', '*');
-        next()
-    })
+
+app.use(bodyParser.urlencoded({ extended: true }))
+// if (mode === 'dev')
+app.all('*', (req, res, next) => {
+    // 设置允许跨域的域名,*代表允许任意域名跨域
+    res.header('Access-Control-Allow-Origin', '*');
+    // 允许的header类型
+    res.header('Access-Control-Allow-Headers', '*');
+    // 跨域允许的请求方式
+    res.header('Access-Control-Allow-Methods', '*');
+    next()
+})
 app.use('/', router)
 
 app.listen(port, () => {
