18f4a4f363c9ca80aceb2f2182a88a48918c87c9
zhantan <zhantan2015@gmail.com>
2022-07-29 10:14:04 +0800

    sql pool

--- src/router/root.ts
+++ src/router/root.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-import mysql from '../models'
+import pool from '../models'
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send(mysql.config);
+    const result = pool.config.connectionConfig.host?.slice(-6)
+    res.send(`<h2>hello ${result}!!!</h2>`);
 })
 
 export default router;
